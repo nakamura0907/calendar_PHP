a24998d915e43cc94aaf52e867568ad30bccefbf
ritsuki0907 <ritsuki990907@gmail.com>
2020-12-24 12:34:17 +0900

    htmlとscssno

--- public/index.php
+++ public/index.php
@@ -6,16 +6,31 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Calendar</title>
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-  <header class="header"></header>
+  <header class="header l-flex l-around">
+
+    <h1 class="header__title">Calendar</h1>
+
+    <nav class="header__nav">
+      <ul class="header__list list l-flex">
+        <li class="list__item"><a href="https://github.com/nakamura0907" target="_blank">nakamura0907</a></li>
+        <li class="list__item"><a href="https://github.com/reonroen" target="_blank">reonroen</a></li>
+      </ul>
+    </nav>
+
+  </header>
 
 
   <main class="main">
+    <h2 class="main__title">YYYY年MM月</h2>
     <table class="table"></table>
   </main>
 
 
-  <footer class="footer"></footer>
+  <footer class="footer">
+    <small class="footer__copyright">&copy; Copyright 2020 Calendar. All rights reserved.</small>
+  </footer>
 </body>
 </html>
