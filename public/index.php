058db9c8397f219c6b17a4e04fee01c3cf82b4e3
reonroen <hm20011027@gmail.com>
2020-12-24 12:36:13 +0900

    カレンダー部分

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+
+  $month=1;
+  $lDay=date('d',strtotime('last day of this month'));
+  $fDate=date('w',strtotime('first day of this month'));
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -12,7 +17,27 @@
 
 
   <main class="main">
-    <table class="table"></table>
+    <table class="table">
+      <tr>
+        <?php
+          for($i=0;$i<$fDate;$i++){
+            echo "<td></td>";
+          }
+          for($i=1;$i<=$lDay;$i++){
+            if($fDate==0&&$i%7==1){
+              echo "</tr><tr>";
+            }
+            if($fDate==1&&$i%7==0){
+              echo "</tr><tr>";
+            }
+            if($i%7==8-$fDate){
+              echo "</tr><tr>";
+            }
+            echo "<td>".$i."</td>";
+          }
+        ?>
+      </tr>
+    </table>
   </main>
 
 
