12a89ca70c903f0f5187fa859de1005bdf815e73
ritsuki0907 <ritsuki990907@gmail.com>
2020-12-24 12:01:11 +0900

    html準備

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calendar</title>
+</head>
+<body>
+  <header class="header"></header>
+
+
+  <main class="main">
+    <table class="table"></table>
+  </main>
+
+
+  <footer class="footer"></footer>
+</body>
+</html>
